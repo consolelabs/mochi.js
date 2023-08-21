6d4ff7ea3968312d9a2f4fa1931b03777922dc50
tuanddd <vincenttuandao@gmail.com>
2023-08-21 15:59:39 +0700

    fix: correct preview api

--- packages/mochi-rest/src/constant.ts
+++ packages/mochi-rest/src/constant.ts
@@ -8,11 +8,11 @@ export const apiUrls = {
     API_PAY_SERVER_URL: `https://api.mochi-pay.console.so/api/v1` as const,
   } as const,
   preview: {
-    API_SERVER_URL: `https://preview-api.mochi.pod.town/api/v1` as const,
+    API_SERVER_URL: `https://api-preview.mochi.pod.town/api/v1` as const,
     API_PROFILE_SERVER_URL:
-      `https://preview-api.mochi-profile.console.so/api/v1` as const,
+      `https://api-preview.mochi-profile.console.so/api/v1` as const,
     API_PAY_SERVER_URL:
-      `https://preview-api.mochi-pay.console.so/api/v1` as const,
+      `https://api-preview.mochi-pay.console.so/api/v1` as const,
   },
 };
 
