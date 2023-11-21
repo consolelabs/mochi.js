dce826413f9f37b2c6a2c2853d20c9235fbe47ec
tuanddd <vincenttuandao@gmail.com>
2023-11-21 22:57:26 +0700

    feat: new pkg mochi-id

---
+++ packages/mochi-id/src/constant.ts
@@ -0,0 +1,5 @@
+export const HOMEPAGE = "https://mochi.gg";
+export const DISCORD_PROFILE = "https://dicordapp.com/users";
+export const TELEGRAM_PROFILE = "tg://user?id=";
+export const MOCHI_API = "https://api.mochi.console.so/api/v1";
+export const MOCHI_PROFILE_API = "https://api.mochi-profile.console.so/api/v1";
